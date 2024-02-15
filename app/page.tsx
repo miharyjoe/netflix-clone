77bb7f7969c62bb4d0830d3d71409f62588d6638
miharyjoel <miharyjoel@gmail.com>
2024-02-15 18:28:34 +0300

    feat: add a redirect if loged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import { authOptions } from "./utils/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  return (
-    <div className="m-5">
-      <h1>
-        Welcome to the netflix clone project
-        <span> {session?.user?.name}</span>
-      </h1>
-      <img src={session?.user?.image} alt="user" />
-    </div>
-  );
+  if (!session) {
+    return redirect("/login");
+  }
+  return redirect("/home");
 }
