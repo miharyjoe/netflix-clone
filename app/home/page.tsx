6361ab6ee015f237838e3f783be1e54b6744245b
miharyjoel <miharyjoel@gmail.com>
2024-02-15 19:36:47 +0300

    feat: add navbar

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,9 @@
+import { NavBar } from "../components/NavBar";
+
 const HomePage = () => {
   return (
     <div className="m-5">
+      <NavBar />
       <h1>Hello authenticated user</h1>
     </div>
   );
